4a99f2cfd556f5a3a38978ccb0663073ddbcf236
Tolu-ojo <toluojoa@student.douglascollege.ca>
2018-10-01 20:51:14 -0700

    Mobile menu added. Optimization progress...20%...

--- js/fancy.js
+++ js/fancy.js
@@ -1,12 +1,28 @@
 $(document).ready(function(){
     $('.container').fadeIn("slow");
 
+
+    //desktop
     $('.desktop-menu-btn').click(function() {
         $('.menu').css("right", "0%").fadeIn("slow");
     });
 
     $('.menu-top > img').click(function() {
         $('.menu').css("right", "-100%").fadeIn("slow");
+    });
+
+
+
+
+
+    //mobile
+
+    $('.menu-btn').click(function() {
+        $('.mobile-menu').fadeIn("slow");
+    });
+
+    $('.mobile-menu-top > img').click(function() {
+        $('.mobile-menu').fadeOut("slow");
     });
 
         
